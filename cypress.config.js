dc2f932e5266336500df70f01887c868d55e8dde
jeniffersouza <ajenifferbsouza@gmail.com>
2025-05-14 23:08:53 -0300

    troquei url

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: 'http://conectalutas.netlify.app',
     supportFile: 'cypress/support/index.js',
     specPattern: 'cypress/e2e/**/*.cy.js',
     setupNodeEvents(on, config) {
