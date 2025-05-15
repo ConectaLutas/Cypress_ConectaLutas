2c322ab78053ccd4c157800e185c1d51fdbb494d
jeniffersouza <ajenifferbsouza@gmail.com>
2025-05-15 09:53:59 -0300

    ajuste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,7 @@ describe('Login', () => {
     cy.get('input#email').type('JENI@GMAIL.COM')
     cy.get('input#password').type('senhaSegura123@')
     cy.get('button[type="submit"]').click();
-    //cy.url().should('include', '/perfil')
+    cy.url().should('include', '/perfil')
    
   })
 })
