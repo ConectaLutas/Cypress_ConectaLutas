265bf2d5c9083193eaeadf11de48ca53323785c1
jeniffersouza <ajenifferbsouza@gmail.com>
2025-05-14 23:31:46 -0300

    xxx

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://conectalutas.netlify.app',
+    baseUrl: 'https://conectalutas.netlify.app',
     supportFile: 'cypress/support/index.js',
     specPattern: 'cypress/e2e/**/*.cy.js',
     setupNodeEvents(on, config) {
